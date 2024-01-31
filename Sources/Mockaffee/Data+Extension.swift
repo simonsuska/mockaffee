fcdd5864fdd955243c6f13422aaf21cb6a3191d8
Simon Suska <bvbnomis@icloud.com>
2024-01-31 18:10:43 +0100

    #5 - implement and document Data+Extension, DescriptionProvider and Frequency

--- Sources/Mockaffee/Data+Extension.swift
+++ Sources/Mockaffee/Data+Extension.swift
@@ -2,8 +2,25 @@ import Foundation
 import CommonCrypto
 
 extension Data {
+    /// An extension property that calculates the sha1 value from the underlying
+    /// `Data` object and returns it in a 40-sign hexadecimal representation.
+    ///
+    /// In the context of the Mockaffee package, it is primarily used to map a
+    /// function call to a sha1 value. Since the function call forms the unique key,
+    /// sha1 is used to prevent collisions to a large extent and reduce the amount
+    /// of storage capacity needed for the most keys.
     var sha1: String {
-        #warning("TODO: Implement")
-        return ""
+        var digest = [UInt8](repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
+        
+        _ = self.withUnsafeBytes { bytes in
+            CC_SHA1(bytes.baseAddress, CC_LONG(self.count), &digest)
+        }
+        
+        var digestHex = ""
+        for index in 0..<Int(CC_SHA1_DIGEST_LENGTH) {
+            digestHex += String(format: "%02x", digest[index])
+        }
+        
+        return digestHex
     }
 }
--- Sources/Mockaffee/DescriptionProvider.swift
+++ Sources/Mockaffee/DescriptionProvider.swift
@@ -1,15 +1,72 @@
 import Foundation
 
+/// A type that provides a description of a given object
 protocol DescriptionProvider {
     func description(of object: Any?) -> String
 }
 
+/// This type provides a description by using the built-in `String(describing:)` mechanism.
+///
+/// In the context of the Mockaffee package, it is used by `Mock` to create an unique
+/// representation of a function call.
 struct DefaultDescriptionProvider: DescriptionProvider {
+    
+    //: MARK: - PROPERTIES
+    
     private static let argumentSeparator = "#"
     private static let valueTypeSeparator = "*"
     
+    //: MARK: - METHODS
+    
+    /// This method provides a description for the specified object.
+    ///
+    /// The description has the following format: `value*type#`. For value types, the
+    /// value is the object itself. For reference types, the value is the unique object
+    /// identifier. Consider the following examples.
+    ///
+    /// ```swift
+    /// let dp = DefaultDescriptionProvider()
+    ///
+    /// enum SomeEnum { case someCase }
+    /// struct SomeStruct { let someVar = 203 }
+    /// class SomeClass {}
+    ///
+    /// dp.description(of: nil) // nil#
+    /// dp.description(of: 174) // 174*Int#
+    /// dp.description(of: SomeEnum.someCase) // someCase*SomeEnum#
+    /// dp.description(of: SomeStruct()) // SomeStruct(someVar: 203)*SomeStruct#
+    /// dp.description(of: SomeClass()) // 105553116333216*SomeClass#
+    /// ```
+    ///
+    /// If two objects produce the same description, they are considered to be equal.
+    ///
+    /// - Parameter object: The object to describe
+    /// - Returns: The description of the given object
     func description(of object: Any?) -> String {
-        #warning("TODO: Implement")
-        return ""
+        guard let object = object else {
+            return "nil\(Self.argumentSeparator)"
+        }
+        
+        let displayStyle = Mirror(reflecting: object).displayStyle
+        let subjectType = Mirror(reflecting: object).subjectType
+        
+        // Since sets and dictionaries are unordered collections, they are sorted
+        // beforehand to guarantee consistency.
+        var description = switch displayStyle {
+            case .class: String(UInt(bitPattern: ObjectIdentifier(object as AnyObject)))
+            case .set: String(describing: (object as! Set<AnyHashable>)
+                .sorted { $0.hashValue < $1.hashValue })
+            case .dictionary: String(describing: (object as! Dictionary<AnyHashable, Any>)
+                .sorted { $0.key.hashValue < $1.key.hashValue })
+            default: String(describing: object)
+        }
+        
+        description.append(
+            Self.valueTypeSeparator +
+            String(describing: subjectType) +
+            Self.argumentSeparator
+        )
+        
+        return description
     }
 }
