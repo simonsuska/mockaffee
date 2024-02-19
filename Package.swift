57240f77787719a313567c8496d967eb1e759c79
Simon Suska <simon.suska@proton.me>
2024-02-19 15:37:41 +0100

    #13 - adjust readme and Package.swift

--- Package.swift
+++ Package.swift
@@ -6,14 +6,11 @@ import PackageDescription
 let package = Package(
     name: "Mockaffee",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Mockaffee",
             targets: ["Mockaffee"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Mockaffee"),
         .testTarget(
